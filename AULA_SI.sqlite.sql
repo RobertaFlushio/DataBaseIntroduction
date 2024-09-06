50b4035ab5f537a561e36a858ff948a7b1842b7c
Roberta Rie Flushio Ohata <160445345+RobertaFlushio@users.noreply.github.com>
2024-09-05 23:15:16 -0300

    comitando QUERIES Sql

---
+++ AULA_SI.sqlite.sql
@@ -0,0 +1,14 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE TB_TESTE_AULA_SI ( 
+	id integer,
+	nome varchar (50),
+	telefone varchar (12)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
---
+++ QUERY_TB_Cliente.sql
@@ -0,0 +1,25 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE TB_Cliente (
+	cpf integer, 
+ 	nome varchar (30), 	
+  	email varchar (50)
+);
+CREATE TABLE TB_Funcionario (
+	cpf integer, 
+ 	nome varchar (30), 	
+  	endereco varchar (100),
+  	funcao varchar (15)
+);
+CREATE TABLE TB_Roupa (
+	codigo_interno integer, 
+ 	tamanho varchar (1), 	
+  	cor varchar (20)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
